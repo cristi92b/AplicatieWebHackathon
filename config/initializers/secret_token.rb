6500f495257fa4f6a35f8bcc9fdd8d6991e0598d
Cristi <cristi92b@gmail.com>
2013-12-14 16:16:46 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GS4::Application.config.secret_key_base = '239110313ce47eb7b0f588ada27b53159b737d5bd84c8d89045422ff9c09d1465ec92421ce9550eb429e3fdfa0c47ad8d6e43a5af512a48c1560bd7b868eb847'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GS4::Application.config.session_store :cookie_store, key: '_GS4_session'
